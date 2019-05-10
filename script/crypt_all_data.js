4f24429a18f94f0f86638b1b6866a86377647cb7
megaratta <noizasm1@hotmail.com>
2019-05-10 17:36:43 +0700

    Update Cryto migrate

--- script/crypt_all_data.js
+++ script/crypt_all_data.js
@@ -7,42 +7,88 @@ const mode = process.env.CRYPTO_MODE
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
-const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL,
+let transports = []
+
+var transport = new (winston.transports.DailyRotateFile)({
+    filename: './migrating-%DATE%.log',
+    //datePattern: 'YYYY-MM-DD-HH',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '30d',
+    json: true,
+    stringify: (obj) => JSON.stringify(obj),
+}).on('rotate', function (oldFilename, newFilename) {
+    // do something fun
 });
 
-db('biometricData').orderBy('timestamp', 'desc')
+transports.push(transport)
+
+if (process.env.LOG_LEVEL === 'debug'){
+    transports.push(new (winston.transports.Console)({
+        json: true,
+        stringify: (obj) => JSON.stringify(obj),
+    }))
+}
+
+const {splat, combine, timestamp, printf} = winston.format;
+
+const myFormat = printf(({timestamp, level, message}) => {
+    return `${timestamp} [${level.toString().toUpperCase()}] ${message} `;
+});
+
+var logger = winston.createLogger({
+    level: process.env.LOG_LEVEL,
+    format: combine(
+        timestamp(),
+        splat(),
+        myFormat
+    ),
+    transports
+});
+
+const select = db('biometricData').orderBy('timestamp', 'desc')
+    .limit(5)
+    .whereNull('ISENCRYPTED')
+
+logger.debug(select.toString())
+
+select
     .catch((err) => {
-        throw err;
+        logger.error(err)
         process.exit(0)
     })
     .then(async (data = []) => {
-
         let en = []
         if (data.length != 0) {
-            for (let index in data){
-                let d = data[index]
-                let enData = await cryptoData('',d.biometricData,mode,logger)
-                en.push({id:d.biometricRefId,data:enData})
+            try {
+                for (let index in data){
+                    let d = data[index]
+                    let enData = await cryptoData('',d.biometricData,mode,logger)
+                    en.push({id:d.biometricRefId,data:enData})
+                }
+            }catch (e) {
+                process.exit(0)
             }
+
         }
         return en
     }).then(async en=>{
-        console.log(JSON.stringify(en))
         for (let index in en) {
             let d = en[index]
             update = db('biometricData')
-                .where('biometricRefId','=',d.id)
-                .update({
-                    biometricData: d.data
-                },['biometricRefId'])
+                .where({biometricRefId:d.id,ISENCRYPTED:null})
+                .update({biometricData: d.data,ISENCRYPTED:1},['biometricRefId'])
+
+            logger.debug(update.toString())
 
             await update
                 .catch(err=>{
-                    throw err
+                    logger.error(err)
+                    process.exit(0)
                 })
                 .then((id)=>{
-                    console.log(`Update ${mode} data biometricRefId : ${id}`)
+                    logger.debug(`Update ${mode} data biometricRefId : ${id}`)
                 })
         }
 
