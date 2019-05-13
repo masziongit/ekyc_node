9a9e5dcb6b92230db3ebb2cb9687be57fcba9cce
megaratta <noizasm1@hotmail.com>
2019-05-13 12:40:54 +0700

    Update Cryto migrate

--- script/crypt_all_data.js
+++ script/crypt_all_data.js
@@ -2,6 +2,11 @@ const db = require('../database/dbconnection').knex
 const cryptoData = require('../service.data.cryptography')
 
 const mode = process.env.CRYPTO_MODE
+const table = 'biometricData'
+
+let whereVal = mode=='encrypt'?0:1
+let updateVal = mode=='encrypt'?1:0
+let limit = 2
 
 // init log
 const winston = require('winston');
@@ -47,50 +52,103 @@ var logger = winston.createLogger({
     transports
 });
 
-const select = db('biometricData').orderBy('timestamp', 'desc')
-    .limit(10)
-    .whereNull('ISENCRYPTED')
+const drop = db.schema.table(table,(tableVal)=> {
+    tableVal.dropColumn('ISENCRYPTED')
+})
 
-logger.debug('SQL : '+select.toString())
+async function migration() {
 
-select
-    .catch((err) => {
-        logger.error(err)
-        process.exit(0)
-    })
-    .then(async (data = []) => {
-        let en = []
-        if (data.length != 0) {
-            try {
-                for (let index in data){
-                    let d = data[index]
-                    let enData = await cryptoData('',d.biometricData,mode,logger)
-                    en.push({id:d.biometricRefId,data:enData})
+const select = db(table).orderBy('timestamp', 'desc')
+    .limit(limit)
+    .where({ISENCRYPTED:whereVal})
+
+    logger.debug('SQL : '+select.toString())
+
+    await select
+        .catch((err) => {
+            logger.error(err)
+            process.exit(0)
+        })
+        .then(async (data = []) => {
+
+            let en = []
+            if (data.length != 0) {
+                try {
+                    for (let index in data) {
+                        let d = data[index]
+                        let enData = await cryptoData('', d.biometricData, mode, logger)
+                        en.push({id: d.biometricRefId, data: enData})
+                    }
+                } catch (e) {
+                    process.exit(0)
                 }
-            }catch (e) {
+
+            }else {
+                logger.info(`No data found in query`)
+                logger.info(`Exit program`)
+
+               await drop.then(()=>{
+                    logger.info(`Drop SQL : ${drop.toString()}`)
+                    db.commit
+                })
+
                 process.exit(0)
             }
-
-        }
-        return en
-    }).then(async en=>{
+            return en
+        }).then(async en => {
         for (let index in en) {
             let d = en[index]
-            update = db('biometricData')
-                .where({biometricRefId:d.id,ISENCRYPTED:null})
-                .update({biometricData: d.data,ISENCRYPTED:1},['biometricRefId'])
+            update = db(table)
+                .where({biometricRefId: d.id, ISENCRYPTED: whereVal})
+                .update({biometricData: d.data, ISENCRYPTED:updateVal}, ['biometricRefId'])
 
-            logger.debug('SQL : '+update.toString())
+            logger.debug('SQL : ' + update.toString())
 
             await update
-                .catch(err=>{
+                .catch(err => {
                     logger.error(err)
                     process.exit(0)
                 })
-                .then((id)=>{
-                    logger.debug(`Update ${mode} data biometricRefId : ${id}`)
+                .then((id) => {
+                    logger.info(`Update ${mode} data biometricRefId : ${id}`)
                 })
         }
 
+        // process.exit(0)
+    })
+
+}
+
+async function loopMigration() {
+
+    const alter = db.schema.table(table,(tableVal)=> {
+        tableVal.integer('ISENCRYPTED',1).notNullable().default(0)
+        db.commit
+    })
+
+    await alter.then(()=>{
+        logger.info(`Alter SQL : ${alter.toString()}`)
+    }).catch(async(err)=>{
+        logger.error(err)
+        await drop.then(()=>{
+            logger.info(`Drop SQL : ${drop.toString()}`)
+            db.commit
+        })
+
         process.exit(0)
     })
+
+    while (true){
+        let loop = await migration()
+    }
+
+    process.exit(0)
+}
+
+
+
+
+loopMigration()
+
+
+
